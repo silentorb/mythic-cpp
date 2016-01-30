5904495273727491d818a0f4196ff972adae510d
cj <christopherwjohnson@gmail.com>
2016-01-30 16:42:24 -0700

    Something changed the permissions of all of the files but it does not matter much either way on Windows.

--- projects/core/metahub/test/metahub_test.cpp
+++ projects/core/metahub/test/metahub_test.cpp
@@ -1,5 +1,17 @@
 #include "metahub_test.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/CallingConv.h"
+#include "llvm/IR/Verifier.h"
+//#include "llvm/Assembly/PrintModulePass.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
 
 int main(int argc, char *const argv[]) {
+  Module* mod = new Module("test", getGlobalContext());
+  delete mod;
   return 0;
 }
