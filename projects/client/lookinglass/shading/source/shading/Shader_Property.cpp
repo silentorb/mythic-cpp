8c6a2a323dc280c1040f5421e22337096d3fc272
cj <cj@silentorb.com>
2016-03-19 19:24:03 -0600

    Finally created Shader_Property that stores the uniform index instead of constantly querying it.

--- projects/client/lookinglass/shading/source/shading/Shader_Property.cpp
+++ projects/client/lookinglass/shading/source/shading/Shader_Property.cpp
@@ -1,5 +1,22 @@
 #include "Shader_Property.h"
+#include "lookinglass/glow.h"
 
 namespace shading {
 
+  Shader_Property::Shader_Property(const string &name, Program &program) :
+    name(name) {
+    uniform_index = glGetUniformLocation(program.get_id(), name.c_str());
+  }
+
+  void Shader_Property::set_matrix(const mat4 &value) {
+    if (uniform_index != -1) {
+      glUniformMatrix4fv(uniform_index, 1, GL_FALSE, (float *) &value);
+    }
+  }
+
+  void Shader_Property::set_vec4(float x, float y, float z, float w) {
+    if (uniform_index != -1) {
+      glUniform4f(uniform_index, x, y, z, w);
+    }
+  }
 }
